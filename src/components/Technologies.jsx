c3a183767e8dfcb0371d533d623ed1ad83872608
Carlos Martin Quispe <carlosquispe1805@gmail.com>
2025-08-13 18:30:41 -0500

    Agregar naimaciones
    Se utilizó Framer Motion para proporcionar mayor fluidez al desplazar en la página

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,33 +5,79 @@ import { SiSpring } from "react-icons/si";
 import { SiSpringboot } from "react-icons/si";
 import { SiMysql } from "react-icons/si";
 import { SiMongodb } from "react-icons/si";
+import { motion } from "framer-motion"
+
+const iconVariants = (duration) => ({
+    initial: { y: -10 },
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        },
+    }
+})
 
 const Technologies = () => {
   return (
     <div className='border-b border-neutral-800 pb-24'>
-        <h2 className='my-20 text-center text-4xl'>
+        <motion.h2 
+            whileInView={{ opacity:1, y: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 1.5 }}
+            className='my-20 text-center text-4xl'>
             Technologies
-        </h2>
-        <div className='flex flex-wrap items-center justify-center gap-4'>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.h2>
+        <motion.div 
+            whileInView={{ opacity:1, x:0 }}
+            initial={{ opcity: 0, x: -100 }}
+            transition={{ duration: 0.5 }}
+            className='flex flex-wrap items-center justify-center gap-4'>
+            <motion.div 
+                variants={iconVariants(2.5)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <RiReactjsLine className='text-7xl text-cyan-400'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div
+                variants={iconVariants(4)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiAngular className='text-7xl text-red-600'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(3)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiSpring className='text-7xl text-lime-500'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(5)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiSpringboot className='text-7xl text-lime-500'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(2)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiMysql className='text-7xl text-sky-700'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(6)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiMongodb className='text-7xl text-green-500'/>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     </div>
   )
 }
