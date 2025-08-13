f6064d00cbe4ddec5e57d04a6ebc2cfd72fe0fa8
Carlos Martin Quispe <carlosquispe1805@gmail.com>
2025-08-13 01:21:18 -0500

    Primer commit para agregar el navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from "./components/NavBar"
+
+const App = () => {
+  return (
+    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
+      <div className='flixed top-0 -z-10 h-full w-full'></div>
+      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className='container mx-auto px-8'>
+        <NavBar />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from "../assets/kevinRushLogo.png"
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa6'
+
+
+const NavBar = () => {
+  return (
+    <nav className='mb-20 flex items-center justify-between py-6'>
+        <div className='flex flex-shrink-0 items-center'>
+            <img className='mx-2 w-10' src={logo} alt="logo" />
+        </div>
+        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+            <FaLinkedin />
+            <FaGithub />
+            <FaInstagram />
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
